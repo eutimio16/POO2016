f38c0838cc4a999de91e9b42fb3ac2575582f7eb
eutimio16 <eutimio@live.com.mx>
2017-05-03 17:45:30 -0500

    Add files via upload

--- JuegoPanel.java
+++ JuegoPanel.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -10,7 +11,11 @@ import javax.swing.JPanel;
 
 public class JuegoPanel extends JPanel implements KeyListener, Runnable{
 	 private int [] vx1 = {380, 350, 410};
-     private int [] vy1 = {70, 140, 140};
+     private int [] vy1 = {75, 140, 140};
+      
+     private int angulo;
+     
+     
 	public JuegoPanel(){
 	super();
 	this.setPreferredSize(new Dimension(1920,1080));
@@ -28,10 +33,13 @@ public class JuegoPanel extends JPanel implements KeyListener, Runnable{
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		Graphics2D g2d =(Graphics2D)g;
+			
 		
-		 g.setColor(Color.WHITE);
-	     g.fillPolygon (vx1, vy1, 3);
+		 g2d.setColor(Color.WHITE);
 	     
+	     g2d.rotate(Math.toRadians(angulo), vx1[0]-10, vy1[0]-10);
+	     g2d.fillPolygon (vx1, vy1, 3);
 		
 	}
 
@@ -45,6 +53,7 @@ public class JuegoPanel extends JPanel implements KeyListener, Runnable{
 		this.vy1[0]=a;
 		this.vy1[1]=b;
 		this.vy1[2]=c;
+		
 	}
 	//runnable
 	public void run() {
@@ -69,14 +78,11 @@ public class JuegoPanel extends JPanel implements KeyListener, Runnable{
 	}
 
 	public void moveRIGHT(){
-		this.vx1[0]+=10;
-		this.vx1[1]+=10;
-		this.vx1[2]+=10;
+		angulo+=15;
+			
 	}
 	public void moveLEFT(){
-		this.vx1[0]-=10;
-		this.vx1[1]-=10;
-		this.vx1[2]-=10;
+		angulo-=15;
 		
 	}
 
@@ -94,12 +100,12 @@ public class JuegoPanel extends JPanel implements KeyListener, Runnable{
 			}
 		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
 			moveRIGHT();
-			setX1(vx1[0],vx1[1],vx1[2]);
+			//setX1(vx1[0],vx1[1],vx1[2]);
 			repaint();
 			}
 		if(e.getKeyCode() == KeyEvent.VK_LEFT){
 			moveLEFT();
-			setX1(vx1[0],vx1[1],vx1[2]);
+			//setX1(vx1[0],vx1[1],vx1[2]);
 			repaint();
 			}
 		
